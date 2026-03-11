be94c7171cd73756f44a579caa3153c100187d28
Sun_Island <2557578904@qq.com>
2026-03-11 12:56:03 +0800

    remove build files

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_qqmusic-island_FILE /mnt/shared/myProject/qqmusic-island/build/qqmusic-island)
-set(__QT_DEPLOY_TARGET_qqmusic-island_TYPE EXECUTABLE)
--- build/CMakeFiles/qqmusic-island_autogen_timestamp_deps.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for qqmusic-island_autogen_timestamp_deps.
